e6bad036450b913319bad7f8ad77d289f6828ab6
Camilly <169068284+costacamilly@users.noreply.github.com>
2024-05-14 20:01:00 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+public static void main(String[] args) {
+System.out.println("                        Curso desenvolvimento de sistemas ");
+   System.out.println("___________________________________________________________________________________");
+System.out.println(" ");
+
+System.out.println("O que é Análise e Desenvolvimento de Sistemas? O curso de Tecnologia em Análise e Desenvolvimento de Sistemas é uma graduação de nível tecnológico que torna o estudante apto a projetar, testar, implantar e gerenciar sistemas e recursos que envolvem Hardware e Software.");
+System.out.println(" ");
+System.out.println("-O curso de desenvolvimento de Sistemas até agora me mostrou conhecimentos, diferentes linguagens e coisas que eu não sabia. As matérias que temos nesse ano são Analise e Projeto de Sistemas, Ciências da Computação e Lógica Computacional");
+System.out.println(" "); 
+System.out.println("-Os professores que temos nos ajudam bastante e demonstram todo esforço para ajudar os alunos. Lembrando que o nosso técnico é junto ao ensino médio ");
+System.out.println(" ");
+System.out.println("-Espero aprender muito mais nesses três próximos anos, e também sair com uma boa formação! ");
+  }
+
+
+}
